002652e4ebd11e97b9341f1d58c6988771918579
your Name <mohamedabdulayubm@gmail.com>
2022-07-03 12:38:44 +0530

    [Ayub]Validate First Name

--- Registration_Regex/Program.cs
+++ Registration_Regex/Program.cs
@@ -1,2 +1,12 @@
-﻿
+﻿using Registration_Regex;
 Console.WriteLine("Registraion problem Using Regex");
+
+
+
+
+
+//To Validate First Name
+ 
+RegexPattern Rp = new RegexPattern();
+var result = Rp.Validate();
+Console.WriteLine(result);
---
+++ Registration_Regex/RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Registration_Regex
+{
+    public class RegexPattern
+    {
+        /// <summary>
+        /// Name starts with Capital
+        /// Length Min 3 Char
+        /// </summary>
+         string FirstName = "^[A-Z]{1}[a-zA-Z]{3,10}$";
+         string First = Console.ReadLine();
+        public bool Validate()
+        { 
+            return Regex.IsMatch(First, FirstName);
+        }
+       
+    }
+}
+
+
+

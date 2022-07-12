307860ea113873f610b4b90cfba451edc094bbe4
your Name <mohamedabdulayubm@gmail.com>
2022-07-12 17:35:28 +0530

    [Ayub] Handled Exception for All the Cases

--- Regex_Testing/UnitTest1.cs
+++ Regex_Testing/UnitTest1.cs
@@ -47,6 +47,41 @@ namespace Regex_Testing
                 bool res = check4.ValidatePassword("Khan235@45");
                 Assert.AreEqual(exp, res);
             }
+        [TestMethod]
+        public void FirstName()
+        {
+            Regex_Pattern_Exp regex = new Regex_Pattern_Exp();
+            string res = regex.FirstName("Ayub");
+            Assert.AreEqual("Correct", res);
         }
+        [TestMethod]
+        public void LastName()
+        {
+            Regex_Pattern_Exp regex = new Regex_Pattern_Exp();
+            string res = regex.LastName("Khan");
+            Assert.AreEqual("Correct", res);
+        }
+        [TestMethod]
+        public void Email()
+        {
+            Regex_Pattern_Exp regex = new Regex_Pattern_Exp();
+            string result = regex.Email("Ayub@bl.co.in");
+            Assert.AreEqual("Correct", result);
+        }
+        [TestMethod]
+        public void MobileNumber()
+        {
+            Regex_Pattern_Exp regex = new Regex_Pattern_Exp();
+            string res = regex.PhoneNumber("91 9874563120");
+            Assert.AreEqual("Correct", res);
+        }
+        [TestMethod]
+        public void Password()
+        {
+            Regex_Pattern_Exp regex = new Regex_Pattern_Exp();
+            string res = regex.Password("Ayub@7");
+            Assert.AreEqual("Correct", res);
+        }
+    }
     }
 }
---
+++ Registration_Regex/Exception_Handling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registration_Regex
+{
+        public enum CustomExp
+        {
+
+            Invalid_First_Name, Invalid_Last_Name, Invalid_Email, Invalid_Phone_Number, Invalid_Password
+
+        }
+        public class Exception_Handling : Exception
+        {
+            public CustomExp ex;
+            public Exception_Handling(string msg, CustomExp ex) : base(msg)
+            {
+                this.ex = ex;
+            }
+        }
+    }
+}
